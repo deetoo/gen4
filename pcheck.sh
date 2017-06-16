976f61ad2c9e87a4f4b1bd19649d6bcc5e26ec09
Doug Dobies <doug.dobies@gmail.com>
2017-06-15 21:45:36 -0500

    better parsing in pcheck

--- pcheck.sh
+++ pcheck.sh
@@ -6,15 +6,18 @@ MFILE="/home/fhadmin/migrate.txt"
 echo "Updating VMware tools."
 /usr/bin/vmware-config-tools.pl -d
 
-echo "DISK INFO" >$MFILE
+echo "--- DISK INFO" >$MFILE
 df -kh >>$MFILE
+echo "--- DISK INFO ENDS" >>$MFILE
 
-echo "IP INFO">>$MFILE
+echo "--- IP INFO">>$MFILE
 ip addr >> $MFILE
+echo "--- IP INFO ENDS" >>$MFILE
 
-echo "PORT INFO" >>$MFILE
+echo "--- PORT INFO" >>$MFILE
 nmap -P0 localhost >>$MFILE
+echo "--- PORT INFO ENDS" >>$MFILE
 
-echo "/tmp NOEXEC check" >>$MFILE
+echo "--- NOEXEC CHECK" >>$MFILE
 cat /proc/mounts |grep -i noexec >>$MFILE
-echo "END" >>$MFILE
+echo "--- NOEXEC check ENDS" >>$MFILE
