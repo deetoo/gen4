ad9aee15a53f927c1438f51a8269616d13b5fe9c
Doug Dobies <doug.dobies@gmail.com>
2017-06-17 22:53:07 -0500

    switched from nmap to netstat and added network connectivity

--- pcheck.sh
+++ pcheck.sh
@@ -6,7 +6,11 @@ MFILE="/home/fhadmin/migrate.txt"
 echo "Updating VMware tools."
 /usr/bin/vmware-config-tools.pl -d
 
-echo "--- DISK INFO" >$MFILE
+echo "--- KERNEL INFO" >$MFILE
+uname -ra >>$MFILE
+echo  --- KERNEL INFO ENDS$'\n\n' >>$MFILE
+
+echo "--- DISK INFO" >>$MFILE
 df -kh >>$MFILE
 echo --- DISK INFO ENDS$'\n\n' >>$MFILE
 
@@ -15,9 +19,12 @@ ip addr |grep "inet "|grep eth >> $MFILE
 echo --- IP INFO ENDS$'\n\n' >>$MFILE
 
 echo "--- PORT INFO" >>$MFILE
-nmap -P0 localhost >>$MFILE
+netstat -plant >>$MFILE
 echo --- PORT INFO ENDS$'\n\n' >>$MFILE
 
+echo "--- OUTBOUND CONNECTIVITY" >>$MFILE
+ping -c3 -W5 google.com >>$MFILE
+echo --- OUTBOUND ENDS$'\n\n' >>$MFILE
 echo "--- NOEXEC CHECK" >>$MFILE
 cat /proc/mounts |grep -i noexec |grep tmp>>$MFILE
 echo --- NOEXEC check ENDS$'\n\n' >>$MFILE
