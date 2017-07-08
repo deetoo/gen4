661b0b2c30650de12fc8434dc7a3f7f4bf78c772
Doug Dobies <doug.dobies@gmail.com>
2017-07-08 01:22:56 -0500

    added armor-vmware-tools internal scripts

--- pcheck.sh
+++ pcheck.sh
@@ -22,6 +22,61 @@ if [ -f /media/VMwareTools-10.1.7-5541682.tar.gz ]
 		echo "VMware Tools not found in /media - exiting!"
 		exit 0
 	fi
+#
+# check for Distro
+#
+	if [ -f /etc/redhat-release ]
+		then
+			echo "This is a RHEL/CentOS box."
+			cat > /etc/init.d/armor-vmware-tools << EOF
+#! /bin/sh
+### BEGIN INIT INFO
+# Provides:          armor-vmware-tools
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Armor VMware tools automatic update script
+# Description:       Armor VMware tools automatic update script
+### END INIT INFO
+#
+# Identifies new kernel versions, kicks off a reconfigure if a new version is detected
+# Discover kernel versions in Guest OS
+find /boot -type f -iname 'initramfs*' -print -exec bash -c "KERNEL=\$(echo {} | sed -e 's/^.*initramfs\-//' -e 's/\.img.*$//')
+    test -f /boot/\$KERNEL.vmtools && echo Skipping \$KERNEL || (echo Building for \$KERNEL
+    vmware-config-tools.pl -d -k \"\$KERNEL\" && touch /boot/\$KERNEL.vmtools)" \;
+EOF
+			chmod 755 /etc/init.d/armor-vmware-tools
+			chkconfig armor-vmware-tools on
+			service armor-vmware-tools
+		fi
+
+	if [ -f /etc/debian_version ]
+		then
+			echo "This is a Debian/Ubuntu box."
+			cat > /etc/init.d/armor-vmware-tools << EOF
+#! /bin/sh
+### BEGIN INIT INFO
+# Provides:          armor-vmware-tools
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Armor VMware tools automatic update script
+# Description:       Armor VMware tools automatic update script
+### END INIT INFO
+#
+# Identifies new kernel versions, kicks off a reconfigure if a new version is detected
+# Discover kernel versions in Guest OS
+find /boot -type f -iname 'initrd*' -print -exec bash -c "KERNEL=\$(echo {} | sed -e 's/^.*initrd\-//')
+    test -f /boot/\$KERNEL.vmtools && echo Skipping \$KERNEL || (echo Building for \$KERNEL
+    vmware-config-tools.pl -d -k \"\$KERNEL\" && touch /boot/\$KERNEL.vmtools)" \;
+EOF
+			chmod 755 /etc/init.d/armor-vmware-tools
+			update-rc.d armor-vmware-tools
+			service armor-vmware-tools
+		fi
+
 
 	find /etc/{apt,yum,yum.repos.d} -type f -not -iname "*.bak*" -print -exec sed -i.bak.$RANDOM -e "s/[A-Za-z][A-Za-z][A-Za-z]01-pkg01\.firehost\.net/a.svc.armor.com/g" {} \;
 
